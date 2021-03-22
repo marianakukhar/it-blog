56fe15d48958cf79f1798b9050f0364bd5bbfdbe
Mariana Kukhar <marianakukhar28@gmail.com>
2021-03-22 23:28:15 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarModule } from 'ng-sidebar'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SidebarModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
